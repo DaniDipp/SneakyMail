60e88f9761a8964a28a71a5259aa9d69e35ad11d
DaniDipp <dippigames@gmail.com>
2024-10-14 01:25:22 +0200

    Initial Commit

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,6 +3,7 @@ plugins {
 }
 
 repositories {
+	mavenCentral()
 	maven {
 		url = uri("https://plugins.gradle.org/m2/")
 	}
@@ -12,7 +13,6 @@ repositories {
     maven {
 		url = uri("https://repo.mikeprimm.com/")
 	}
-	mavenCentral()
 	maven("https://repo.papermc.io/repository/maven-public/")
 }
 
@@ -22,15 +22,14 @@ dependencies {
 	// compileOnly("me.clip:placeholderapi:2.11.5")
     // compileOnly("us.dynmap:dynmap-api:3.4-beta-3")
     // compileOnly("us.dynmap:DynmapCoreAPI:3.4")
-    // implementation("com.squareup.okhttp3:okhttp:4.12.0")
-    // implementation("org.json:json:20240303")
+	implementation("io.github.agrevster:pocketbase-kotlin:2.6.2")
 }
 
 tasks.jar {
 	manifest {
-		attributes["Main-Class"] = "com.danidipp.paperplugin"
+		attributes["Main-Class"] = "com.danidipp.sneakymail"
 	}
-
+	duplicatesStrategy = DuplicatesStrategy.EXCLUDE
 	from(configurations.runtimeClasspath.get().map { if (it.isDirectory) it else zipTree(it) })
 }
 
--- src/main/kotlin/com/danidipp/sneakymail/SneakyMail.kt
+++ src/main/kotlin/com/danidipp/sneakymail/SneakyMail.kt
@@ -1,8 +1,8 @@
-package com.danidipp.paperplugin
+package com.danidipp.sneakymail
 
 import org.bukkit.plugin.java.JavaPlugin
 
-class PaperPlugin : JavaPlugin() {
+class SneakyMail : JavaPlugin() {
 
     override fun onLoad() {
         instance = this
@@ -12,12 +12,12 @@ class PaperPlugin : JavaPlugin() {
     }
 
     companion object {
-        const val IDENTIFIER = "paperplugin"
+        const val IDENTIFIER = "sneakymail"
         const val AUTHORS = "Team Sneakymouse"
         const val VERSION = "1.0"
-        private lateinit var instance: PaperPlugin
+        private lateinit var instance: SneakyMail
 
-        fun getInstance(): PaperPlugin {
+        fun getInstance(): SneakyMail {
             return instance
         }
     }
